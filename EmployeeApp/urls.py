f6e041eecc794c5e3c56db043df688a2332b2043
juan <juansabas.10@gmail.com>
2023-01-04 10:25:29 -0300

    first commit

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,14 @@
+from EmployeeApp import views
+from django.urls import re_path,path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+    re_path(r'^department$',views.departmentApi),
+    re_path(r'^department/([0-9]+)$',views.departmentApi),
+    
+    re_path(r'^employee$',views.employeeApi),
+    re_path(r'^employee/([0-9]+)$',views.employeeApi),
+    
+    re_path(r'^employee/savefile',views.SaveFile)
+]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
